15799078409a3abf9aecce306d7ad789593605d6
AleexxG <alexanderg.sln@gmail.com>
2022-11-03 23:13:12 +0100

    tenzies-layout

---
+++ src/comp/Dice.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Dice() {
+  return (
+    <div className='dice'>
+        <h2 className='dice-num'>1</h2>
+    </div>
+  );
+}
+
+export default Dice;
---
+++ src/comp/Text.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Text() {
+  return (
+    <div className='text'>
+        <h2 className='title'>Tenzies</h2>
+        <p className='instruction'>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+    </div>
+  )
+}
+
+export default Text;
