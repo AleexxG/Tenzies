2b50d3d9db33b21b6ad4cb5f7e6a2eb1e15b8e78
AleexxG <alexanderg.sln@gmail.com>
2022-11-11 23:21:08 +0100

    made dice

--- src/comp/Dice.js
+++ src/comp/Dice.js
@@ -5,13 +5,86 @@ function Dice(props) {
     backgroundColor: props.isHeld ? 'var(--details-color)' : 'white'
   }
 
-  return (
-    <div className='dice' 
+let value
+  if (props.value === 1) {
+    return (
+      <div className='dice' 
           onClick={() => props.holdDice(props.id)} 
           style={styles}>
+        <h2 className='dice-num'>
+          <span className='dot'></span>
+        </h2>
+      </div>
+    )
+  } else if (props.value === 2) {
+    return (
+      <div className='dice' 
+          onClick={() => props.holdDice(props.id)} 
+          style={styles}>
+        <h2 className='dice-num'>
+          <span className='dot'></span>
+          <span className='dot'></span>
+        </h2>
+      </div>
+    )
+  } else if (props.value === 3) {
+    return (
+      <div className='dice' 
+          onClick={() => props.holdDice(props.id)} 
+          style={styles}>
+        <h2 className='dice-num'>
+          <span className='dot'></span>
+          <span className='dot'></span>
+          <span className='dot'></span>
+        </h2>
+      </div>
+    )
+  } else if (props.value === 4) {
+    return (
+      <div className='dice' 
+          onClick={() => props.holdDice(props.id)} 
+          style={styles}>
+        <h2 className='dice-num'>
+          <span className='dot'></span>
+          <span className='dot'></span>
+          <span className='dot'></span>
+          <span className='dot'></span>
+        </h2>
+      </div>
+    )
+  } else if (props.value === 5) {
+    return (
+      <div className='dice' 
+          onClick={() => props.holdDice(props.id)} 
+          style={styles}>
+        <h2 className='dice-num'>
+          <span className='dot'></span>
+          <span className='dot'></span>
+          <span className='dot'></span>
+          <span className='dot'></span>
+          <span className='dot'></span>
+        </h2>
+      </div>
+    )
+  } else if (props.value === 6) {
+    return (
+      <div className='dice' 
+          onClick={() => props.holdDice(props.id)} 
+          style={styles}>
+        <h2 className='dice-num'>
+          <span className='dot'></span>
+          <span className='dot'></span>
+          <span className='dot'></span>
+          <span className='dot'></span>
+          <span className='dot'></span>
+          <span className='dot'></span>
+        </h2>
+      </div>
+    )
+  }
 
-        <h2 className='dice-num'>{props.value}</h2>
-    </div>
+  return (
+    {value}
   );
 }
 
